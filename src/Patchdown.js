c34e9723fb13e3c3bf72aca3d8d2279c30007a7f
Michael Bock <no-day@posteo.net>
2025-04-22 20:22:11 +0200

    Refactor

--- src/Patchdown.js
+++ src/Patchdown.js
@@ -1,22 +1,12 @@
-export const replaceEffectImpl = function (just) {
-  return function (nothing) {
-    return function (r) {
-      return function (f) {
-        return function (s) {
-          return function () {
-            return s.replace(r, function (match) {
-              var groups = [];
-              var group,
-                i = 1;
-              while (typeof (group = arguments[i++]) !== "number") {
-                groups.push(group == null ? nothing : just(group));
-              }
-              return f(match)(groups)();
-            });
-          };
-        };
-      };
-    };
-  };
-};
+export const replaceEffectImpl = (just, nothing, r, f, s) => {
+  s.replace(r, (match) => {
+    const groups = [];
+    let group,
+      i = 1;
+    while (typeof (group = arguments[i++]) !== "number") {
+      groups.push(group == null ? nothing : just(group));
+    }
 
+    return f(match, groups);
+  });
+};
--- src/Patchdown.purs
+++ src/Patchdown.purs
@@ -10,24 +10,28 @@ import Control.Monad.Except (ExceptT, runExceptT)
 import Data.Argonaut (class EncodeJson, encodeJson)
 import Data.Argonaut.Core (Json)
 import Data.Array (foldMap)
+import Data.Array as Array
 import Data.Bifunctor (lmap)
 import Data.Codec.Argonaut (JsonDecodeError, printJsonDecodeError)
 import Data.Codec.Argonaut as CA
 import Data.Either (Either(..))
-import Data.Foldable (fold)
+import Data.Foldable (fold, sum)
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Maybe (Maybe(..), fromMaybe')
 import Data.String as Str
 import Data.String.Regex (Regex, regex)
 import Data.String.Regex.Flags as RegFlags
-import Data.Tuple.Nested ((/\))
+import Data.Tuple.Nested (type (/\), (/\))
 import Effect (Effect)
 import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Exception (Error, error, message)
+import Effect.Ref as Ref
+import Effect.Uncurried (EffectFn2, EffectFn5, mkEffectFn2, runEffectFn5)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile, writeTextFile)
 import Node.Process (lookupEnv)
+import Node.Process as Process
 import Partial.Unsafe (unsafeCrashWith)
 import Patchdown.Common (Converter, ConvertError, codeBlock, logInfo, mdH5, mdQuote, print, printYaml, runConverter, yamlToJson)
 import Patchdown.Converters.Purs (mkConverterPurs)
@@ -49,7 +53,7 @@ replacePdSection
   -> ( { converterName :: String, yamlStr :: String, enable :: Boolean }
        -> Effect { newYamlStr :: String, newContent :: String, errors :: Array ConvertError }
      )
-  -> Effect String
+  -> Effect { str :: String, countErrors :: Int }
 replacePdSection content replaceFn = do
   let regexStr = mkStartTag "([a-zA-Z0-9_]*)(\\!?)\\s([\\s\\S]*?)" <> "[\\s\\S]*?" <> endTag
 
@@ -57,7 +61,7 @@ replacePdSection content replaceFn = do
     # lmap (\_ -> error "invalid regex")
     # liftEither
 
-  replaceEffect reg
+  (str /\ vals) <- replaceEffect reg
     ( \_ matches -> do
         { converterName, enable, yamlStr } <-
           case matches of
@@ -66,14 +70,19 @@ replacePdSection content replaceFn = do
 
         { newContent, newYamlStr, errors } <- replaceFn { converterName, yamlStr, enable }
 
-        pure $ fold
-          [ mkStartTag (converterName <> (if enable then "" else "!") <> "\n" <> newYamlStr)
-          , foldMap (\err -> errorBoxImpl converterName (err.message) err.value) errors
-          , newContent
-          , endTag
-          ]
+        let
+          str = fold
+            [ mkStartTag (converterName <> (if enable then "" else "!") <> "\n" <> newYamlStr)
+            , foldMap (\err -> errorBoxImpl converterName (err.message) err.value) errors
+            , newContent
+            , endTag
+            ]
+
+        pure (str /\ Array.length errors)
     )
     content
+
+  pure { str, countErrors: sum vals }
 
 data Err
   = InvalidYaml { err :: String }
@@ -119,7 +128,7 @@ getReplacement converterMap { converterName, yamlStr, enable } = do
           { newContent: content, newYamlStr, errors }
     )
 
-run :: Opts -> Effect Unit
+run :: Opts -> Effect { countErrors :: Int }
 run { filePath, converters } = do
   let converterMap = converters # map (\c -> runConverter c _.name /\ c) # Map.fromFoldable
   let converterNames = Map.keys converterMap
@@ -128,7 +137,7 @@ run { filePath, converters } = do
 
   fileContent <- readTextFile UTF8 filePath
 
-  patchedFileContent <-
+  { str: patchedFileContent, countErrors } <-
     replacePdSection fileContent \opts@{ converterName, yamlStr } -> do
       ret <- runExceptT $ getReplacement converterMap opts
 
@@ -168,6 +177,8 @@ run { filePath, converters } = do
 
   writeTextFile UTF8 filePath patchedFileContent
 
+  pure { countErrors }
+
 main :: Effect Unit
 main = do
   filePath <- lookupEnv "PATCHDOWN_FILE_PATH"
@@ -183,18 +194,35 @@ main = do
       , converters
       }
 
-  run opts
+  { countErrors } <- run opts
+
+  unless (countErrors == 0) do
+    Process.exit' 1
 
 foreign import replaceEffectImpl
-  :: (forall r. r -> Maybe r)
-  -> (forall r. Maybe r)
-  -> Regex
-  -> (String -> Array (Maybe String) -> Effect String)
-  -> String
-  -> Effect String
+  :: EffectFn5
+       (forall r. r -> Maybe r)
+       (forall r. Maybe r)
+       Regex
+       (EffectFn2 String (Array (Maybe String)) String)
+       String
+       String
 
-replaceEffect :: Regex -> (String -> Array (Maybe String) -> Effect String) -> String -> Effect String
-replaceEffect = replaceEffectImpl Just Nothing
+replaceEffect :: forall a. Regex -> (String -> Array (Maybe String) -> Effect (String /\ a)) -> String -> Effect (String /\ Array a)
+replaceEffect reg replFn oldStr = do
+  refVals <- Ref.new []
+  newStr <- runEffectFn5 replaceEffectImpl Just Nothing reg
+    ( mkEffectFn2 \match matches -> do
+        (str /\ val) <- replFn match matches
+        Ref.modify_ (_ <> [ val ]) refVals
+        pure str
+    )
+    oldStr
+
+  vals <- Ref.read refVals
+  pure (newStr /\ vals)
+
+--replaceEffectImpl Just Nothing
 
 errorBox :: forall a. EncodeJson a => String -> String -> a -> String
 errorBox sectionName message val = errorBoxImpl sectionName message (Just $ encodeJson val)
